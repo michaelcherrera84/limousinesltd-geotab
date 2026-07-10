3f437541e8cba108f7f9bdea103ed295e49672d1
Michael C. Herrera <michael.c.herrera@icloud.com>
2026-07-09 19:10:43 -0500

    Update for GH Pages.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,27 @@
-import { createFileRoute, Link } from '@tanstack/react-router';
+import { createFileRoute, Link, useRouter } from '@tanstack/react-router';
+import { useEffect } from 'react';
+import { geotabConnect } from '@/auth/geotab-connect.ts';
 
 export const Route = createFileRoute('/')({ component: LandingPage });
 
 function LandingPage() {
+    const router = useRouter();
+
+    useEffect(() => {
+        const getSession = async () => {
+            try {
+                await geotabConnect();
+                await router.navigate({ to: '/reports' });
+            } catch (error) {
+                console.error('Error connecting to Geotab:', error);
+                await router.navigate({ to: '/login' });
+            }
+        };
+        void getSession();
+    }, []);
+
     return (
-        <div className="flex w-full h-screen flex-col p-16">
+        <div className="flex h-screen w-full flex-col p-16">
             <header className="flex h-32 flex-col items-center justify-center gap-4">
                 <h1 className="text-xl font-bold">Welcome to the Limousines LTD Add-in</h1>
                 <p>
@@ -12,7 +29,7 @@ function LandingPage() {
                     individual urls.
                 </p>
             </header>
-            <main className="flex flex-col items-center h-full justify-center">
+            <main className="flex h-full flex-col items-center justify-center">
                 <Link to="/login" className="btn-primary w-fit px-4! shadow-md">
                     Login
                 </Link>
